6c7dfb771d67f4625af598d8b0f85c5e94d2cd90
mutu <mutluerturk@gmail.com>
2020-01-24 12:22:56 +0000

    login-logout

--- eksi_engelle.py
+++ eksi_engelle.py
@@ -1,17 +1,23 @@
-/**
+"""
  Entry favorileyen tüm suserler için otomatik olarak engelle ve başlaklarını engelle 
  istekleri gönderilir.
-**/
+"""
 
 # Todo: 
 # error handling, 
 # command prompt parameter read, 
-# login configuration file,
+# cursor integration
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 
 # TODO: imput from command line
+
+USERNAME = input("email adresi : ")
+PASSWORD = input("password : ")
+TARGET_SUSER = input('target suser :')
+
+URL_LOGIN = 'https://eksisozluk.com/giris?'
 URL_SUSER = 'https://eksisozluk.com/biri/'
 
 class eksi_engelle():
@@ -22,26 +28,31 @@ class eksi_engelle():
         #todo: headles true ?
         opts.headless = False
         self.browser = Firefox(options = opts)
-        self.browser.get(URL_USER + '') # todo first parameter of run command
-        self.suser = []
+        #self.browser.get(URL_USER + '') # todo first parameter of run command
+        #self.username = USERNAME
+        #self.password = PASSWORD
+        #self.target_suser = TARGET_SUSER
+        #self.suser = []
         
     def log_in(self):
         #todo login
-        self.browser.find_element_by_id('username').send_keys("xxxxxx")  # login
-        self.browser.find_element_by_id('password').send_keys("xxxxxx")  # password
-        self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/section/div/form/fieldset/div[4]/button').click()
+        self.browser.get(URL_LOGIN)
+        self.browser.find_element_by_id('username').send_keys(USERNAME)  # login
+        self.browser.find_element_by_id('password').send_keys(PASSWORD)  # password
+        self.browser.find_element_by_css_selector('button.btn.btn-primary.btn-lg.btn-block').click()
 
     def log_out(self):
         #todo logout
         # pop up menu
-        self.browser.find_element_by_xpath('/html/body/header/div/nav[1]/ul/li[9]/a').click()
+        self.browser.find_element_by_css_selector('#options-dropdown > a:nth-child(1)').click()
         # terk link
-        self.browser.find_element_by_xpath('/html/body/header/div/nav[1]/ul/li[9]/ul/li[6]/a').click()
+        self.browser.find_element_by_css_selector('li.separated:nth-child(6) > a:nth-child(1)').click()
 
     def collectFavs(self):
         #self.browser.find_element_by_xpath() # todo second parameter of run command
         #suser main page
-        self.browser.get('https://eksisozluk.com/biri/marlon-brandonun-kedisi')
+        #self.browser.get('https://eksisozluk.com/biri/marlon-brandonun-kedisi')
+        self.browser.get(URL_SUSER + TARGET_SUSER)
         #suser fav page
         self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/section/div[3]/ul/li[2]/a').click()
         #suser faved entrys favs
@@ -62,7 +73,7 @@ class eksi_engelle():
     def runEngelleToCollectedUsers(self):
         while suser in suserCollection:
             # open susers page in new tab
-            new_url = 'https://eksisozluk.com/biri/' + suser
+            new_url = 'URL_SUSER' + suser
             new_tab = "window.open('" + new_url + "\',  \'new tab\')"
             self.browser.execute_script(new_tab)
 
@@ -85,3 +96,9 @@ class eksi_engelle():
         self.log_out()
         # close susers tab
         browser.close()
+
+if __name__ == "__main__":
+    run = eksi_engelle()
+    run.log_in()
+    run.log_out()
+    
